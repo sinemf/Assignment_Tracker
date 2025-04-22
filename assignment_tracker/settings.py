754b6f232e441625a649b8d310a53585400c693a
sinemf <sinemf04@gmail.com>
2025-04-22 00:44:18 -0400

    Update settings.py

--- assignment_tracker/settings.py
+++ assignment_tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3wrm_b=9kxu)v!y=f1pgcsbrso8xdc_8^b_!(l_-pz+qm*%2q8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [assignment-tracker-hc5j.onrender.com]
+ALLOWED_HOSTS = ["assignment-tracker-hc5j.onrender.com"]
 
 
 # Application definition
