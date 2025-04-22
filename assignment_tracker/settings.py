c8095a8c99d7e914f43e70e74e45ec67851f4442
sinemf <sinemf04@gmail.com>
2025-04-22 01:26:12 -0400

    Update settings.py

--- assignment_tracker/settings.py
+++ assignment_tracker/settings.py
@@ -118,9 +118,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tracker/static')]  # <- source
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')            # <- output
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tracker/static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
